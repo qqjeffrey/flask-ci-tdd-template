b00a7c3789b17101eab8ad50ddd8048cbcebea20
qqjeffrey <jeffrey890705@gmail.com>
2025-03-31 02:36:37 +0800

    新增 hello 測試（尚未實作）

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,3 +2,8 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == {"message": "Hello, World!"}
+
+def test_hello(client):
+    response = client.get('/hello/Jeff')
+    assert response.status_code == 200
+    assert response.json == {"message": "Hello, Jeff!"}
